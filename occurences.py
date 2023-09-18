8b1319361b7367ce209081ccfa073e97491914d6
Dhaval Fatnani <39245649+DhavalFatnani@users.noreply.github.com>
2023-09-18 13:09:18 +0000

    Problem Statement 1 Solution

---
+++ occurences.py
@@ -0,0 +1,35 @@
+"""
+Problem 1: Program to count occurrences of a given character in a string.
+
+ Count character 'e' in the below string.
+	Input "geeksforgeeks".
+	Output: 4
+
+	Count character '3' in the below string.
+	Input "abccdefgaa."
+	Output : 3
+"""
+
+
+class CountOccurances:
+    def __init__(self, string, character):
+        self.string = string
+        self.character = character
+        self.count = 0
+
+    def counter(self):
+        for i in range(len(self.string)):
+            if self.string[i] == self.character:
+                self.count += 1
+
+        return f"Character '{self.character}' in {self.string} occurs {self.count} times."
+
+i_in_india = CountOccurances("india",'i')
+print(i_in_india.counter())
+
+
+e_in_geeksforgeeks = CountOccurances("geeksforgeeks", 'e')
+print(e_in_geeksforgeeks.counter())
+
+_3_in_abccdefgaa = CountOccurances("abccdefgaa.", '3')
+print(_3_in_abccdefgaa.counter())
