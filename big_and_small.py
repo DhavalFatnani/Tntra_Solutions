43a29d9a2de901ca527e02c12881df0677072d7a
Dhaval Fatnani <39245649+DhavalFatnani@users.noreply.github.com>
2023-09-18 13:30:41 +0000

    3 Problem statement added

---
+++ big_and_small.py
@@ -0,0 +1,14 @@
+"""
+Problem 3:  Biggest and smallest number in an array
+Write a program to print the biggest and smallest number in the array. 
+
+Input: arr[] = {1, 2, 3, 4, 5}
+Output: Maximum is: 5
+Minimum is: 1
+
+Input: arr[] = {5, 3, 7, 4, 2}
+Output: Maximum is: 7
+Minimum is: 2
+
+"""
+
