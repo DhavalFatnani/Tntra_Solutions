5de979373964af72d3fae3cd6d62de546d2b2c01
Dhaval Fatnani <39245649+DhavalFatnani@users.noreply.github.com>
2023-09-18 14:25:40 +0000

    Problem statement 3 completed

---
+++ big_and_small.java
@@ -0,0 +1,57 @@
+/*
+Problem 3:  Biggest and smallest number in an array
+Write a program to print the biggest and smallest number in the array. 
+
+Input: arr[] = {1, 2, 3, 4, 5}
+Output: Maximum is: 5
+Minimum is: 1
+
+Input: arr[] = {5, 3, 7, 4, 2}
+Output: Maximum is: 7
+Minimum is: 2
+
+*/
+
+import java.util.*;
+
+public class big_and_small{
+    public static void main(String[] args) {
+        
+        //Arrays
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {5, 3, 7, 4, 2};
+
+        ArrayAnalyzer analyzer2 = new ArrayAnalyzer(arr2);
+        ArrayAnalyzer analyzer1 = new ArrayAnalyzer(arr1);
+
+        String result1 = analyzer1.find();
+        String result2 = analyzer2.find();
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
+
+class ArrayAnalyzer{
+    private int[] array;
+
+    public ArrayAnalyzer(int[] array){
+        this.array = array;
+    }
+
+    public String find(){
+        int biggest = array[0];
+        int smallest = array[0];
+
+        for(int i=0; i < array.length; i++){
+            if(array[i]>biggest){
+                biggest = array[i];
+            }
+            else if(array[i]<smallest){
+                smallest = array[i];
+            }
+        }
+
+        return String.format("In array %s, Biggest Value: %d and Smallest Value: %d", Arrays.asList(array).toString(), biggest, smallest);
+    }
+}
